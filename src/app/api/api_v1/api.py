5886c9c74a111ef98c947755d729260885de7d13
Kristiyan Tashev <kristiyan.tashev@gfk.com>
2023-09-22 13:06:04 +0300

    add db

--- src/app/api/api_v1/api.py
+++ src/app/api/api_v1/api.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter
 
 from app.api.api_v1.endpoints import (
-    hello_world,
+    hello_world, items,
 )
 
 api_router = APIRouter()
@@ -11,4 +11,10 @@ api_router.include_router(
     hello_world.router,
     prefix="/hello-world",
     tags=["Hello World"],
+)
+
+api_router.include_router(
+    items.router,
+    prefix="",
+    tags=["Users"],
 )
